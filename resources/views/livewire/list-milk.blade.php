48ed7724bbceed83bd520eb58540cb8c240887ac
Kyle B. Johnson <kyle.johnson@hey.com>
2024-10-11 23:37:53 -0400

    Add icons for storage types

--- resources/views/livewire/list-milk.blade.php
+++ resources/views/livewire/list-milk.blade.php
@@ -9,11 +9,19 @@
                         <p class="text-xl font-semibold leading-6 text-gray-900">
                             {{ $milk->amount }} ounces
                         </p>
+                        <p class="mt-1 text-xs leading-5 text-gray-500">{{ $milk->created_at }}</p>
                     </div>
                 </div>
                 <div class="shrink-0 sm:flex sm:flex-col sm:items-end">
-                    <p class="text-sm leading-6 text-gray-900">{{ $milk->storage_type }}</p>
-                    <p class="mt-1 text-xs leading-5 text-gray-500">{{ $milk->created_at }}</p>
+{{--                    <p class="text-sm leading-6 text-gray-900">{{ $milk->storage_type }}</p>--}}
+                    @if($milk->storage_type == 'fridge')
+                        <img width="40px" src="{{ asset('storage/icon_fridge.png') }}" alt="">
+                    @elseif($milk->storage_type == 'frozen')
+                        <img width="40px" src="{{ asset('storage/icon_frozen.png') }}" alt="">
+                    @elseif($milk->storage_type == 'thawed')
+                        <img width="40px" src="{{ asset('storage/icon_thaw.png') }}" alt="">
+                    @endif
+
                 </div>
             </li>
 
